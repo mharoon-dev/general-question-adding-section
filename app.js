68e399eaa965adb900acc71f54231e6dcd0c73f4
mharoon-dev <haroon.aghani@gmail.com>
2023-10-22 15:08:31 +0500

    complete

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
-let onBtn = document.querySelector('.on');
-let offBtn = document.querySelector('.off');
-let p = document.querySelector('.p');
-offBtn.style.display = 'none';
-p.style.display = 'none';
-p.style.opacity = '0';
-p.style.transition = 'opacity 2s'; // Corrected the transition property
+// card 1
 
-function on() {
-    onBtn.style.display = 'none';
-    offBtn.style.display = 'inline'; // Corrected this line
+let onBtn1 = document.querySelector('.on1');
+let offBtn1 = document.querySelector('.off1');
+let p = document.querySelector('.p1');
+offBtn1.style.display = 'none';
+p.style.display = 'none';
+
+function on1() {
+    onBtn1.style.display = 'none';
+    offBtn1.style.display = 'inline';
     p.style.display = 'inline';
-    p.style.opacity = '1';
+}
+function off1() {
+    onBtn1.style.display = 'inline';
+    offBtn1.style.display = 'none';
+    p.style.display = 'none';
 }
 
-function off() {
-    onBtn.style.display = 'inline';
-    offBtn.style.display = 'none';
-    p.style.display = 'none';
-    p.style.opacity = '0';
+// card 2
+
+let onBtn2 = document.querySelector('.on2');
+let offBtn2 = document.querySelector('.off2');
+let p2 = document.querySelector('.p2');
+offBtn2.style.display = 'none';
+p2.style.display = 'none';
+
+function on2() {
+    onBtn2.style.display = 'none';
+    offBtn2.style.display = 'inline';
+    p2.style.display = 'inline';
+}
+function off2() {
+    onBtn2.style.display = 'inline';
+    offBtn2.style.display = 'none';
+    p2.style.display = 'none';
+}
+
+// card 3
+
+let onBtn3 = document.querySelector('.on3');
+let offBtn3 = document.querySelector('.off3');
+let p3 = document.querySelector('.p3');
+offBtn3.style.display = 'none';
+p3.style.display = 'none';
+
+function on3() {
+    onBtn3.style.display = 'none';
+    offBtn3.style.display = 'inline';
+    p3.style.display = 'inline';
+}
+function off3() {
+    onBtn3.style.display = 'inline';
+    offBtn3.style.display = 'none';
+    p3.style.display = 'none';
 }
